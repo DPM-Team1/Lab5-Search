4cf0b3e686e8c2cee6da4c675056c67bdcedb95b
alexnorms <alexa.normandin@mail.mcgill.ca>
2018-10-19 10:26:43 -0400

    Lab 5 main class implementation

--- src/ca/mcgill/ecse211/Lab5/Lab5.java
+++ src/ca/mcgill/ecse211/Lab5/Lab5.java
@@ -3,6 +3,8 @@ package ca.mcgill.ecse211.Lab5;
 import lejos.hardware.Button;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
@@ -12,7 +14,17 @@ import lejos.robotics.SampleProvider;
 
 public class Lab5 {
 
+	
+	private static final EV3LargeRegulatedMotor leftMotor =
+		      new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
+	private static final EV3LargeRegulatedMotor rightMotor =
+		      new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
+	
 	private static final Port colorPort = LocalEV3.get().getPort("S4");	
+	
+	private static final TextLCD lcd = LocalEV3.get().getTextLCD();
+	//public static final double WHEEL_RAD = 2.2;
+	//public static final double TRACK = 9.7;
 
 	public static void main(String[] args) {
 
